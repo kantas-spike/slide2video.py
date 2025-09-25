8f64058a64b30c1fb42706dc941cf9f27cfb2872
kanta <101532773+kantas-spike@users.noreply.github.com>
2025-09-26 07:47:56 +0900

    entrypointではbpyに依存しないように修正

--- src/slide2video/__init__.py
+++ src/slide2video/__init__.py
@@ -2,7 +2,9 @@ import argparse
 import os
 import subprocess
 
-from . import blender
+
+def get_init_script_path_for_blender():
+    return os.path.join(os.path.dirname(__file__), "blender", "__init__.py")
 
 
 def get_etc_dir():
@@ -19,13 +21,22 @@ def main_short() -> None:
     args, remaining = parse_args()
     short_settings_path = os.path.join(get_etc_dir(), "settings_for_short.json")
     for_short_args = ["-c", short_settings_path]
-    cli_args = [args.blender, "-P", blender.__file__, "--"] + for_short_args + remaining
+    cli_args = (
+        [args.blender, "-P", get_init_script_path_for_blender(), "--"]
+        + for_short_args
+        + remaining
+    )
     # print("slide2video_short: ", cli_args)
     subprocess.run(cli_args)
 
 
 def main() -> None:
     args, remaining = parse_args()
-    cli_args = [args.blender, "-P", blender.__file__, "--"] + remaining
+    cli_args = [
+        args.blender,
+        "-P",
+        get_init_script_path_for_blender(),
+        "--",
+    ] + remaining
     # print("slide2video: ", cli_args)
     subprocess.run(cli_args)
